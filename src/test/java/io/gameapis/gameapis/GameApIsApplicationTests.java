d1eddb0eac871a307e4e06e4c573fe31cacbbfa4
Gabriele C <sgdc3.mail@gmail.com>
2018-11-17 11:56:23 +0100

    Initial commit

---
+++ src/test/java/io/gameapis/gameapis/GameApIsApplicationTests.java
@@ -0,0 +1,15 @@
+package io.gameapis.gameapis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameApIsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
